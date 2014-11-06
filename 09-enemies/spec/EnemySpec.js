6805e606138fed6da11fd767aef6a0565cec4992
kb8alvaro <kb8_alvaro@hotmail.com>
2014-11-06 16:26:49 +0100

    Realizado el test de constructor de enemy

--- 09-enemies/spec/EnemySpec.js
+++ 09-enemies/spec/EnemySpec.js
@@ -62,14 +62,77 @@
 */
 
 
+describe("Prueba Enemy", function(){
+
+	beforeEach(function(){
+		loadFixtures('index.html');
+		s = SpriteSheet;
+		g = Game;	
+	});
+	afterEach(function(){
+		SpriteSheet = s;
+		Game = g;	
+	});
 
 
+	it("Enemy factory", function(){
+		SpriteSheet = { map: {
+					missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+					enemy_test:   { sx: 37, sy: 0, w: 42, h: 43, frames: 1 }
+					 } };
+		var enemies = {
+			basic: { x: 100, y: -50, sprite: 'enemy_test', B: 100, C: 2 , E: 100 }
+		};
+		var e1 = new Enemy(enemies.basic);
+		expect(e1.x).toEqual(100);
+		var e2 = new Enemy(enemies.basic, { x: 200 });
+		expect(e2.x).toEqual(200);
+	});
 
 
+});
 
+/*
+Enemy.prototype.step = function(dt) {
+    // Actualizamos la edad
+    this.t += dt;
 
+    // El patrón de movimiento lo dictan las ecuaciones que se utilizarán
+    // para calcular las componentes x e y de su velocidad: vx e vy:
+    
+    // vx tiene una componente constante A, y otra que va variando
+    // cíclicamente en función de la edad del enemigo (t), según la
+    // sinuisoide definida por las constantes B, C y D.
+    // A: componente constante de la velocidad horizontal
+    // B: fuerza de la velocidad horizontal sinusoidal
+    // C: periodo de la velocidad horizontal sinusoidal
+    // D: desplazamiento en el tiempo de la velocidad horizontal sinusoidal
+    this.vx = this.A + this.B * Math.sin(this.C * this.t + this.D);
+    
+    // vy tiene una componente constante E, y otra que va variando
+    // cíclicamente en función de la edad del enemigo (t), según la
+    // sinuisoide definida por las constantes F, G y H.
+    // E: componente constante de la velocidad vertical
+    // F: fuerza de la velocidad vertical sinusoidal
+    // G: periodo de la velocidad vertical sinusoidal
+    // H: desplazamiento en el tiempo de la velocidad vertical sinusoidal
+    this.vy = this.E + this.F * Math.sin(this.G * this.t + this.H);
 
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
 
+    if(this.y > Game.height ||
+       this.x < -this.w||
+       this.x > Game.width) {
+	this.board.remove(this);
+    }
+}
 
+// Metodo draw, que anadimos al prototipo para que cada instancia de
+// Enemy no tenga una copia de el
+Enemy.prototype.draw = function(ctx) {
+    SpriteSheet.draw(ctx,this.sprite,this.x,this.y);
+}
 
+*/
 
